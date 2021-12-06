b35dacb635308cadf91a01b708e931f514cdee43
Wouter Paesen <wouter@daemontech.be>
2021-12-06 13:27:08 +0100

    1000 days

--- 2021/06/aoc.cpp
+++ 2021/06/aoc.cpp
@@ -9,14 +9,16 @@
 #include <stdexcept>
 #include <iomanip>
 #include <list>
+// 386755
 
 class MotherFish
 {
 public:
+
+    using valueType = unsigned __int128;
+
     MotherFish()
     {
-       
-
         spawn.fill(0);
         state = 0;
         for (size_t i=0;i<9; ++i) children.push_back(0);
@@ -45,9 +47,9 @@ public:
         }
     }
 
-    int64_t amount()
+    valueType amount()
     {
-        int64_t ret = 0;
+        valueType ret = 0;
         for (auto &sp : spawn)
         {
             ret += sp;
@@ -59,10 +61,31 @@ public:
         return ret;
     }
 
+    std::string amountAsString()
+    {
+        std::string ret;
+
+        valueType value = amount();
+        while (value > 0)
+        {
+            std::array<char, 3> buf;
+
+            unsigned digit = value % 10;
+            value = value / 10;
+
+            std::snprintf(buf.data(), buf.size(), "%u", digit);
+            ret.append(buf.data());
+        }
+
+        std::reverse(ret.begin(), ret.end());
+
+        return ret;
+    }
+
 private:
 
-    std::array<int64_t, 7> spawn;
-    std::list<int64_t> children;
+    std::array<valueType, 7> spawn;
+    std::list<valueType> children;
     int state; 
 };
 
@@ -94,13 +117,13 @@ main(int argc, char **argv)
         exit(-1);
     }
 
-    for ( size_t i=0; i<256; ++i )
+    for ( size_t i=0; i<=1000; ++i )
     {   
         mothers.tick();
 
-        if ( ( ( i+1 ) == 80) || ( ( i+1 ) == 256) )
+        if ( ( i== 80) || ( i == 256) || ( i == 1000 ) )
         {
-            std::cout << "After " << i+1 << " days : " << mothers.amount() << std::endl;
+            std::cout << "After " << i << " days : " << mothers.amountAsString() << std::endl;
         }
     }
 
